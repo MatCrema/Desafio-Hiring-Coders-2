5142864ef29b7139cf78b7afcdfa2075f73f589a
Matheus <matheus_crema@hotmail.com>
2021-07-25 16:44:34 -0300

    Mudança no baseURL, adicionado /produtos

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/MatCrema/Desafio-Hiring-Coders-2'
+    baseURL: 'https://my-json-server.typicode.com/MatCrema/Desafio-Hiring-Coders-2/produtos'
 })
 
 export default api;
