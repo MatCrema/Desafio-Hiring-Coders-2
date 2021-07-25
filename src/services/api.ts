9229dcdeaf28ab1f3b35afe4cd365726e7495be2
Matheus <matheus_crema@hotmail.com>
2021-07-25 16:40:42 -0300

    Mudança no nome do arquivo server.json para db.json, mudança no baseURL dentro do api.ts e mudança no comando server dentro do package.json

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8888/produtos'
+    baseURL: 'https://my-json-server.typicode.com/MatCrema/Desafio-Hiring-Coders-2'
 })
 
 export default api;
